0b6ae0a2d6d8eeec9904b71b01854da19ce3e063
Victor Reis <victorsireis@gmail.com>
2024-01-14 22:53:30 -0300

    refactor: date filter

--- src/components/DateFilter.tsx
+++ src/components/DateFilter.tsx
@@ -1,61 +1,72 @@
-import React from 'react'
+import React from "react";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "./ui/Select";
 
 interface DateFilterProps {
-  selectedMonth: number
-  selectedYear: number
-  onChangeMonth: (event: React.ChangeEvent<HTMLSelectElement>) => void
-  onChangeYear: (event: React.ChangeEvent<HTMLSelectElement>) => void
+	selectedMonth: number;
+	selectedYear: number;
+	onChangeMonth: (month: string) => void;
+	onChangeYear: (year: string) => void;
 }
 
 const DateFilter: React.FC<DateFilterProps> = ({
-  selectedMonth,
-  selectedYear,
-  onChangeMonth,
-  onChangeYear,
+	selectedMonth,
+	selectedYear,
+	onChangeMonth,
+	onChangeYear,
 }) => {
-  const translatedMonths = [
-    'Janeiro',
-    'Fevereiro',
-    'Março',
-    'Abril',
-    'Maio',
-    'Junho',
-    'Julho',
-    'Agosto',
-    'Setembro',
-    'Outubro',
-    'Novembro',
-    'Dezembro',
-  ]
-  const currentYear = new Date().getFullYear()
-  const startYear = currentYear - 10
+	const translatedMonths = [
+		"Janeiro",
+		"Fevereiro",
+		"Março",
+		"Abril",
+		"Maio",
+		"Junho",
+		"Julho",
+		"Agosto",
+		"Setembro",
+		"Outubro",
+		"Novembro",
+		"Dezembro",
+	];
+	const currentYear = new Date().getFullYear();
+	const startYear = currentYear - 10;
 
-  return (
-    <section className="flex">
-      <select
-        value={selectedMonth}
-        onChange={onChangeMonth}
-        className="select select-secondary select-sm mr-2"
-      >
-        {translatedMonths.map((mes, index) => (
-          <option key={index + 1} value={index + 1}>
-            {mes}
-          </option>
-        ))}
-      </select>
-      <select
-        value={selectedYear}
-        onChange={onChangeYear}
-        className="select select-secondary select-sm"
-      >
-        {Array.from({ length: 11 }, (_, i) => startYear + i).map((year) => (
-          <option key={year} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
-    </section>
-  )
-}
+	return (
+		<section className="flex gap-2">
+			<Select
+				onValueChange={(value) => onChangeMonth(value)}
+				defaultValue={selectedMonth.toString()}
+			>
+				<SelectTrigger className="w-[180px]">
+					<SelectValue placeholder="Mes" />
+				</SelectTrigger>
+				<SelectContent>
+					{translatedMonths.map((mes, index) => (
+						<SelectItem value={(index + 1).toString()}>{mes}</SelectItem>
+					))}
+				</SelectContent>
+			</Select>
+			<Select
+				onValueChange={(value) => onChangeYear(value)}
+				defaultValue={selectedYear.toString()}
+			>
+				<SelectTrigger className="w-[180px]">
+					<SelectValue placeholder="Ano" />
+				</SelectTrigger>
+				<SelectContent>
+					{Array.from({ length: 11 }, (_, i) => startYear + i).map((year) => (
+						<SelectItem value={year.toString()}>{year}</SelectItem>
+					))}
+				</SelectContent>
+			</Select>
+		</section>
+	);
+};
 
-export default DateFilter
+export default DateFilter;
