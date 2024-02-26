1066530a36b36e1aa48db8bbcb4f28f67f85b929
Victor Reis <victorsireis@gmail.com>
2024-02-26 12:15:15 -0300

    build: datatablepagination

--- src/components/transactions/DataTablePagination.tsx
+++ src/components/transactions/DataTablePagination.tsx
@@ -5,7 +5,7 @@ import {
   DoubleArrowRightIcon,
 } from '@radix-ui/react-icons'
 import { Table } from '@tanstack/react-table'
-import { Button } from '../ui/Button'
+import { Button } from '@/components/ui/button'
 import {
   Select,
   SelectContent,
